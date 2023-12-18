3b6d7b62915aa253afd03bdbbd05206702f94c95
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-12-18 18:07:24 +0530

    isort applied

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 html_text = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=").text
 soup = BeautifulSoup(html_text, 'lxml')
